e772d6d9f1e76470b7d0b94b7dfa8c96fdd0170f
egg-tree <matthew.roman@student.allenisd.org>
2022-10-05 08:35:21 -0500

    master commit

---
+++ main.swift
@@ -0,0 +1,31 @@
+var word = String()
+var unsortedArray = [String]()
+
+// Add your code below:
+var line : String?
+repeat {
+
+    line = readLine()
+    if line != nil {
+        word.append(line!)
+    }
+    unsortedArray.append(word)
+    word.removeAll()
+} while line != nil
+
+
+func sort(array: [String]) {
+
+    var fake = array
+    let count = fake.count
+
+    for i in  0 ..< count {
+        for j in 0 ..< count - i - 1 {
+            if fake[j] > fake[j + 1] {
+                fake.swapAt(j, j + 1)
+            }
+        }
+    }
+    print(fake) 
+}
+sort(array: unsortedArray)
